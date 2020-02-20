289cd98c6ff65c50c9605b7c3512bfaf6ca14c85
Surface Pro <Surface Pro@WenceSurfaceTab>
2020-02-20 21:25:33 +0300

    Updated Cash Receipt - Other Revenue2

--- vp/scrpt_db_upgrade_06.sql
+++ vp/scrpt_db_upgrade_06.sql
@@ -5,6 +5,10 @@ alter table item add column override_gen_name int(1) default 0; -- 0 DoNotOverri
 INSERT INTO parameter_list (parameter_list_id, context, parameter_name, parameter_value, description) VALUES (53, 'COMPANY_SETTING', 'OUTPUT_SHOW_CO_NAME', '1','');
 INSERT INTO transaction_reason (transaction_reason_id, transaction_reason_name, transaction_type_id) VALUES (115, 'OTHER REVENUE', 14);
 
+
+
+-- DO NOT RUN  THE ONES BELO---
+
 -- 17th Feb 2020
 alter table discount_package add column store_scope varchar(500) default '';
 alter table discount_package add column transactor_scope varchar(500) default ''; 
