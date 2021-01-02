a2ba96d47d4fda6bead4d3179ab85758c44a8216
Wence <Wence@UHSS-WB>
2021-01-02 18:45:36 +0300

    A few changes in the files

--- vp/scrpt_upload_ura_lookups_windows.sql
+++ vp/scrpt_upload_ura_lookups_windows.sql
@@ -1,6 +1,6 @@
 -- 1. check secure l;ocation to upload files from
 SHOW VARIABLES LIKE "secure_file_priv";
--- 2. Upload the csv files to that location: C:/wamp64/tmp
+-- 2. copy and paste the csv files to that location e.g C:/wamp64/tmp
 -- 3. run the following commands
 LOAD DATA INFILE 'C:/wamp64/tmp/unit_tax_list.csv' INTO TABLE `unit_tax_list` FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
 LOAD DATA INFILE 'C:/wamp64/tmp/item_unspsc.csv' INTO TABLE `item_unspsc` FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
